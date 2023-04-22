0a45d6f4eff7cd7c6702c0d0b57378fd9d03ea73
shajn786 <shajn786@gmail.com>
2023-04-22 15:14:32 +0530

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { EmployeeEntryComponent } from './employee-entry/employee-entry.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+const myRoutes: Routes = [
+  {
+     path:"",component:AdminLoginComponent
+  },
+  {
+    path:"employeeentry",component:EmployeeEntryComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    EmployeeEntryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee-entry/employee-entry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-entry',
+  templateUrl: './employee-entry.component.html',
+  styleUrls: ['./employee-entry.component.css']
+})
+export class EmployeeEntryComponent {
+
+}
